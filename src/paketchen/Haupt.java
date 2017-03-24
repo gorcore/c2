a3fbadcb9b3e6d4239ee7a3a4a61f6ee799d60b8
tlutz <tlutz@LUTZ.RESIT.lokal>
2017-03-24 09:40:20 +0100

    Sinnlos.

--- src/paketchen/Haupt.java
+++ src/paketchen/Haupt.java
@@ -3,8 +3,10 @@ package paketchen;
 public class Haupt {
 	public static void main (String[] args) {
 		// Start der Ausgabe folgt
+		// Und noch eines.
 		System.out.println("Allerlei\n");
 		System.out.println("Durcheinander:");
+		// Noch ein Gedicht.
 		Desk desk = new Desk();
 		System.out.println(desk.getDescription());
 	} // main()
