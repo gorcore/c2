52e7a29f0ba6e8a9609d4732731cbc380a1bca80
tlutz <tlutz@LUTZ.RESIT.lokal>
2017-03-24 09:10:17 +0100

    Thilo

--- src/paketchen/Haupt.java
+++ src/paketchen/Haupt.java
@@ -3,7 +3,8 @@ package paketchen;
 public class Haupt {
 	public static void main (String[] args) {
 		// Start der Ausgabe folgt
-		System.out.println("Allerlei");
+		System.out.println("Allerlei\n");
+		System.out.println("Durcheinander:");
 		Desk desk = new Desk();
 		System.out.println(desk.getDescription());
 	} // main()
